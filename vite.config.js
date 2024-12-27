c171f90c2b71b4392aa0b306523686e253015508
Rahul <159545061+iamrahul-l@users.noreply.github.com>
2024-12-27 13:04:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT) || 5173, 
+    host: true, 
+  },
+  build: {
+    outDir: 'dist', 
+  },
+});
